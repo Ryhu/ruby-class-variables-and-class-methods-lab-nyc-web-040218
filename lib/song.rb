38e32eec65c9a938966b3dc10ac6c60ec7d5364a
Ryan Hu <ryanhuuu@gmail.com>
2018-04-06 13:04:55 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song 
+  attr_accessor  :name, :artist, :genre
+  
+  @@count
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genres
+    @@genres
+  end
+end
